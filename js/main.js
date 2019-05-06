480794c6d4350774f666682367b42845398a947d
brandonroesler <roesler19@gmail.com>
2019-05-06 14:58:52 -0500

    update button object array

---
+++ js/main.js
@@ -0,0 +1,51 @@
+/*----- constants -----*/
+const buttonsArray = [
+    {
+        name: '1',
+        src: '../assets/img1.jpg'
+    },
+    {
+        name: '2',
+        src: '../assets/img2.jpg'
+    },
+    {
+        name: '3',
+        src: '../assets/img3.jpg'
+    },
+    {
+        name: '4',
+        src: '../assets/img4.jpg'
+    },
+    {
+        name: '5',
+        src: '../assets/img5.jpg'
+    },
+    {
+        name: '6',
+        src: '../assets/img6.jpg'
+    },
+    {
+        name: '7',
+        src: '../assets/img7.jpg'
+    },
+    {
+        name: '8',
+        src: '../assets/img8.jpg'
+    },
+    {
+        name: '9',
+        src: '../assets/img9.jpg'
+    }
+];
+
+/*----- app's state (variables) -----*/
+const buttonEls = document.createElement('button')
+
+/*----- cached element references -----*/
+const piece1 = document.createElement('div');
+
+/*----- event listeners -----*/
+// document.querySelector('button').addEventListener('click', placePiece);
+
+/*----- functions -----*/
+//init();
